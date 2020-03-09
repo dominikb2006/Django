8b6654f5f9b2c86b18a7be980ec3493a58066538
Dominik <dominikb2006@github.com>
2020-03-09 22:34:03 +0100

    AAaaaaAAA

--- DB/DB-14-Django_1/My_Django_Stuff/first_project/first_app/views.py
+++ DB/DB-14-Django_1/My_Django_Stuff/first_project/first_app/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def index(request):
     # return HttpResponse("Hello World!")
     my_dict = {'insert_me': "Hello I am from first_appviewx.py"}
-    return render(request, 'first_app/help.html', context=my_dict)
+    return render(request, 'first_app/index.html', context=my_dict)
