985ec8c7e30af56f560c1fd712c710d9e48b25ea
BrysDom <dominikb2006@gmail.com>
2020-04-14 17:45:09 +0200

    detail and list views. Settings blog project

--- DB/DB-18-CBV/advcbv/basic_app/admin.py
+++ DB/DB-18-CBV/advcbv/basic_app/admin.py
@@ -1,3 +1,7 @@
+from basic_app.models import School, Student
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.register(School)
+admin.site.register(Student)
--- DB/DB-19-Blog_project/mysite/mysite/urls.py
+++ DB/DB-19-Blog_project/mysite/mysite/urls.py
@@ -1,4 +1,4 @@
-"""advcbv URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -13,13 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from basic_app import views
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.IndexView.as_view()),
-    path('basic_app/', include('basic_app.urls', namespace='basic_app')),
-    # path('', views.CBView.as_view()),
 ]
