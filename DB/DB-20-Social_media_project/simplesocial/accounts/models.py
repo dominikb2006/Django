0d9cbcf4f968fb749109ceadc79eaff7c2b50ea0
BrysDom <dominikb2006@gmail.com>
2020-05-14 16:10:33 +0200

    SM project

---
+++ DB/DB-20-Social_media_project/simplesocial/accounts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib import auth
+
+
+class User(auth.models.User, auth.models.PermissionMixin):
+    def __str__(self):
+        return "@{}".format(self.username)
+
+
